7988c75a813f36d21ca41884a5ef5ffd81d2ce7a
Colin Nelson <orthros@users.noreply.github.com>
2017-09-07 09:09:54 -0500

    Added support for the BetterTTV emotes for the current channel. Also reformatted the main funciton body (should be whitespace only). NOTE: Long term, I am considering putting in a settings.json file to configure the environment as opposed to the environment variables. You can imagine a scenario where you have several of these bots running on a box and while you certainly could containerize them and leverage the environment variables, it might be simpler long term to have them available in a file and use the environment as a backup

--- server.js
+++ server.js
@@ -62,86 +62,101 @@ var betterTTVEmotesPromise = request("https://api.betterttv.net/emotes").then(fu
     } while (emoteMatch);
 });
 //Promise to get all the BetterTTV Emotes for the current channel
+var betterTTVChannelEmotesPromise = request("https://api.betterttv.net/2/channels/" + process.env.TWITCH_CHANNEL).then(function (jsonBody) {
+    var emoteReg = /"code"\s*:\s*"([\w\d\(\)':&]+)"/g;
+    var emoteMatch;
+    do {
+        emoteMatch = emoteReg.exec(jsonBody);
+        if (emoteMatch) {
+            betterTtvEmotes.add(emoteMatch[1].toLowerCase());
+        }
+    } while (emoteMatch);
+});
 
 //Wait for us to read the english words, the twitch emotes and the better ttv emotes
-Promise.all([fileReadPromise, globalTwitchEmotesPromise, subscriberTwitchEmotesPromise, betterTTVEmotesPromise]).then(function () {
-    bot.connect({
-        host: 'irc.chat.twitch.tv',
-        port: 6667,
-        nick: process.env.TWITCH_NICK,
-        password: process.env.TWITCH_TOKEN
-    });
+Promise.all([fileReadPromise,
+    globalTwitchEmotesPromise,
+    subscriberTwitchEmotesPromise,
+    betterTTVEmotesPromise,
+    betterTTVChannelEmotesPromise])
+    .then(function () {
+        bot.connect({
+            host: 'irc.chat.twitch.tv',
+            port: 6667,
+            nick: process.env.TWITCH_NICK,
+            password: process.env.TWITCH_TOKEN
+        });
 
-    bot.on('registered', () => {
-        console.log('registered on twitch irc!');
-        const channel = bot.channel(process.env.TWITCH_CHANNEL);
-        channel.join();
+        bot.on('registered', () => {
+            console.log('registered on twitch irc!');
+            const channel = bot.channel(process.env.TWITCH_CHANNEL);
+            channel.join();
 
-        //We'll match everything for now
-        bot.matchMessage(new RegExp(), (event) => {
-            //simple debug statement to let you know things are working
-            console.log(event.nick + ': ' + event.message);
-            //This would be an interesting place to put in a whitelist of nicks. 
-            //Aka: wow that orthros_ guy is pretty funny && makes puns in other languages
-            //     all the time, we should let him do non english phrases.
+            //We'll match everything for now
+            bot.matchMessage(new RegExp(), (event) => {
+                //simple debug statement to let you know things are working
+                console.log(event.nick + ': ' + event.message);
+                //This would be an interesting place to put in a whitelist of nicks. 
+                //Aka: wow that orthros_ guy is pretty funny && makes puns in other languages
+                //     all the time, we should let him do non english phrases.
 
-            var message = event.message;
-            //Remove all punctuation save for "@"
-            message = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-            //Split the message by whitespace
-            var words = message.split(" ");
-            //Remove all words beginning with "@" as they are username mentions
-            words = words.filter(function (word) {
-                return word.charAt(0) != "@";
+                var message = event.message;
+                //Remove all punctuation save for "@"
+                message = message.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+                //Split the message by whitespace
+                var words = message.split(" ");
+                //Remove all words beginning with "@" as they are username mentions
+                words = words.filter(function (word) {
+                    return word.charAt(0) != "@";
+                });
+                //Remove the emotes
+                words = words.filter(function (word) {
+                    return !twitchEmotes.has(word);
+                });
+                //Then remove the better ttv emotes
+                words = words.filter(function (word) {
+                    return !betterTtvEmotes.has(word);
+                });
+
+                //At this point it would be wise for us to remove the
+                //words that are only digits of some kind
+                const digitReg = /^\d+$/;
+                words = words.filter(function (word) {
+                    return !digitReg.test(word);
+                })
+
+                //Here is a point for some optimization.
+                //If we are at a point where we have < X words
+                //and X is sufficiently low enough (ie 2)
+                //do we really want to do any more processing on it?
+                //This is clearly up for MUCH debate and we should err on the side of caution
+                //but one could imagine a situation where someone simply utters 
+                //     lulz rekd
+                //That probably wont hit the emotes filter and is more than likely 
+                //not in the top 10k english words
+                //We may need to incorporate an english slang dictionary
+                //For now we will leave this alone and leave it as a thought
+
+
+                //Now go through all the words in the top 10k english words
+                //And count how many there are that are NON english
+                var nonEnglishWords = words.filter(function (word) {
+                    return !commonEnglish.has(word.toLowerCase());
+                });
+
+                //If there are any non english we need to do some math
+                if (nonEnglishWords.length > 0) {
+                    //First we'll calculate the ratio of non english words to english words
+                    var ratio = nonEnglishWords.length / words.length;
+
+                    console.log(ratio);
+
+                    //If the ratio is sufficently low (ie .1 or less) we can continue.
+                    //Otherwise it might be nice to call out to the google language
+                    //detection API.
+                    //This costs 20$ for every million characters passed to it, which is why
+                    //we are filtering out emotes etc to begin with
+                }
             });
-            //Remove the emotes
-            words = words.filter(function (word) {
-                return !twitchEmotes.has(word);
-            });
-            //Then remove the better ttv emotes
-            words = words.filter(function (word) {
-                return !betterTtvEmotes.has(word);
-            });
-
-            //At this point it would be wise for us to remove the
-            //words that are only digits of some kind
-            const digitReg = /^\d+$/;
-            words = words.filter(function (word) {
-                return !digitReg.test(word);
-            })
-
-            //Here is a point for some optimization.
-            //If we are at a point where we have < X words
-            //and X is sufficiently low enough (ie 2)
-            //do we really want to do any more processing on it?
-            //This is clearly up for MUCH debate and we should err on the side of caution
-            //but one could imagine a situation where someone simply utters 
-            //     lulz rekd
-            //That probably wont hit the emotes filter and is more than likely 
-            //not in the top 10k english words
-            //We may need to incorporate an english slang dictionary
-            //For now we will leave this alone and leave it as a thought
-
-
-            //Now go through all the words in the top 10k english words
-            //And count how many there are that are NON english
-            var nonEnglishWords = words.filter(function (word) {
-                return !commonEnglish.has(word.toLowerCase());
-            });
-
-            //If there are any non english we need to do some math
-            if (nonEnglishWords.length > 0) {
-                //First we'll calculate the ratio of non english words to english words
-                var ratio = nonEnglishWords.length / words.length;
-
-                console.log(ratio);
-
-                //If the ratio is sufficently low (ie .1 or less) we can continue.
-                //Otherwise it might be nice to call out to the google language
-                //detection API.
-                //This costs 20$ for every million characters passed to it, which is why
-                //we are filtering out emotes etc to begin with
-            }
         });
     });
-});
